1ede42448946f5f22490b15912bde38fd15c1585
Fabio <alessf13@gmail.com>
2021-09-26 15:33:19 +0200

    feat: first color palatte generated

---
+++ app.js
@@ -0,0 +1,34 @@
+const colorDivs = document.querySelectorAll(".color");
+const generateBtn = document.querySelector(".generate");
+const sliders = document.querySelectorAll('input[type="range"]');
+const currentHexes = document.querySelectorAll(".color h2");
+let initialColors;
+
+// Functions
+
+// Color Generator
+
+function generateHex() {
+  // const letters = "#0123456789QBCDEF";
+  // let hash = "#";
+  // for (let i = 0; i < 6; i++) {
+  //   hash += letters[Math.floor(Math.random() * 16)];
+  // }
+  // return hash;
+  const hexColor = chroma.random();
+  return hexColor;
+}
+
+function randomColors() {
+  colorDivs.forEach((div, index) => {
+    const hexText = div.children[0];
+    const randomColor = generateHex();
+
+    // Add the color to the background
+    console.log(randomColor);
+    div.style.backgroundColor = randomColor;
+    hexText.innerText = randomColor;
+  });
+}
+
+randomColors();
